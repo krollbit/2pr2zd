64b09a79abcb230652f1af1579439ebf56414c1c
Nate <nigllemmesmash@gmail.com>
2020-09-18 11:28:37 +0300

    Initial commit

---
+++ src/laba3/Testball.java
@@ -0,0 +1,10 @@
+package laba3;
+
+public class Testball {
+    public static void main(String[] args) {
+        ball b1 = new ball(10, 10);
+        System.out.println(b1);
+        b1.move(33, 45);
+        System.out.println(b1);
+    }
+}
